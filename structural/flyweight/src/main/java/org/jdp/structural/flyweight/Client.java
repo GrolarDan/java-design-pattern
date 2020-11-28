643f62d67f33c8198894df931c230976e7499927
Daniel Mašek <daniel@masci.cz>
2020-11-28 20:52:35 +0100

    Add Spring boot dependency.

--- structural/flyweight/src/main/java/org/jdp/structural/flyweight/Client.java
+++ structural/flyweight/src/main/java/org/jdp/structural/flyweight/Client.java
@@ -1,13 +1,22 @@
 package org.jdp.structural.flyweight;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @Slf4j
-public class Client {
+@SpringBootApplication
+public class Client implements CommandLineRunner {
 
   public static void main(String[] args) {
+    SpringApplication.run(org.jdp.structural.flyweight.Client.class, args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
     SystemErrorMessage msg1 = ErrorMessageFactory.getInstance().getMessage(ErrorMessageFactory.ErrorType.GenericSystemError);
     log.info(msg1.getText("2006"));
   }
-
+  
 }
