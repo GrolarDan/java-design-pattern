edf8233e928c2386213543dccb7bdb689ecc49a8
Daniel Mašek <daniel@masci.cz>
2020-12-27 15:38:09 +0100

    Make Proxy JPD springboot application

--- structural/proxy/src/main/java/org/jdp/structural/proxy/Client.java
+++ structural/proxy/src/main/java/org/jdp/structural/proxy/Client.java
@@ -2,11 +2,20 @@ package org.jdp.structural.proxy;
 
 import javafx.geometry.Point2D;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @Slf4j
-public class Client {
+@SpringBootApplication
+public class Client implements CommandLineRunner {
 
   public static void main(String[] args) {
+    SpringApplication.run(org.jdp.structural.proxy.Client.class, args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
     Image image = ImageFactory.getImage("A1.bmp");
 
     image.setLocation(new Point2D(10, 10));
