fdc146a6ed194d2c0a4272bb876f7ca8585f4d72
Daniel Mašek <daniel@masci.cz>
2020-12-29 11:41:35 +0100

    Chain of responsibility - setup as Spring boot

--- behavioral/chain-of-responsibility/src/main/java/org/jdp/behavioral/responsibilitychain/Client.java
+++ behavioral/chain-of-responsibility/src/main/java/org/jdp/behavioral/responsibilitychain/Client.java
@@ -3,28 +3,37 @@ package org.jdp.behavioral.responsibilitychain;
 import java.time.LocalDate;
 import lombok.extern.slf4j.Slf4j;
 import static org.jdp.behavioral.responsibilitychain.LeaveApplication.Type.PTO;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @Slf4j
-public class Client {
+@SpringBootApplication
+public class Client implements CommandLineRunner {
 
   public static void main(String[] args) {
-    System.out.println(System.getProperty("org.slf4j.simpleLogger.defaultLogLevel"));
+    SpringApplication.run(org.jdp.behavioral.responsibilitychain.Client.class, args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
     LeaveApprover approver = setupApprovers();
     var application = LeaveApplication.builder()
             .type(PTO)
             .from(LocalDate.now())
             .to(LocalDate.now().plusDays(4))
             .build();
-    
+
     log.info("{}", application);
     approver.processRequest(application);
     log.info("{}", application);
   }
 
-  private static LeaveApprover setupApprovers() {
+  private LeaveApprover setupApprovers() {
     Director director = new Director(null);
     Manager manager = new Manager(director);
     ProjectLead lead = new ProjectLead(manager);
     return lead;
   }
+
 }
